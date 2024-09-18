2045fe2ff5d4150783b98d9e2a73ce246df2dfc7
Lucas-biblos <lbiblos@udesa.edu.ar>
2024-09-18 10:15:17 -0300

    terminado el header

--- proyecto-integrador-g17/src/components/Header/Header.js
+++ proyecto-integrador-g17/src/components/Header/Header.js
@@ -2,18 +2,26 @@ import React, { Component } from 'react'
 import Navbar from "../Navbar/Navbar"
 import "./Header.css"
 
+
 class Header extends Component {
-  render() {
-    return (
-      <div>
-        <p>header</p>
-        <Navbar></Navbar>
-      </div>
-    )
-  }
+ render() {
+   return (
+     <div>
+       <Navbar></Navbar>
+     </div>
+   )
+ }
 }
 
 
+
+
 export default Header;
+
+
+
+
+
+
 
 
--- proyecto-integrador-g17/src/components/Navbar/Navbar.js
+++ proyecto-integrador-g17/src/components/Navbar/Navbar.js
@@ -1,19 +1,26 @@
+
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import "./Navbar.css"
 const Navbar = () => {
-    return (
-        <nav>
-            <ul className="main-nav">
-                <li><Link  exact to="/"> Home</Link></li>
-                <li><Link to="/about"> About</Link></li>
-                <li>Elemento menu</li>
-            </ul>
-            <ul className="user">
-                <li>Nombre usuario <img src="./img/user.jpg" alt="User" /></li>
-            </ul>
-        </nav>
-    );
+   return (
+       <nav>
+           <ul className="main-nav">
+           <img src="/img/Logo.jpg" alt="Logo" className='site-logo' />
+               <li className="navbartexto"><Link  exact to="/"> Home</Link></li>
+               <li className="navbartexto"><Link to="/favoritos">Favoritos</Link></li>
+               <li className="navbartexto"><Link to="/ver-todas">Ver Todas</Link></li>
+                  
+           </ul>
+        
+       </nav>
+   );
 };
 
 
 
+
+
+
 export default Navbar
+
+
